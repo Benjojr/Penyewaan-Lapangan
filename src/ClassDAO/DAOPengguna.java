a0c2c02eea77acc856254c6cf65ed79915b2c476
rchmn17 <ptr330rachman@gmail.com>
2025-05-30 07:59:43 +0700

    DAo fix

--- src/ClassDAO/DAOPengguna.java
+++ src/ClassDAO/DAOPengguna.java
@@ -12,24 +12,45 @@ import ConnectionClass.*;
 public class DAOPengguna {
     DatabaseConnection dc = new DatabaseConnection();
     
-    public Pengguna Load(String id) {
-        String sql = "SELECT * FROM users WHERE id = "+id;
+    public Pengguna LoadSome(String id) {
+        String sql = "SELECT * FROM Pengguna WHERE id_Pengguna like '"+id+"'";
         try (Connection conn = dc.getConnection();PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, id);
             try (ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
                     return new Pengguna(
-                        rs.getString("id"),
-                        rs.getString("no_telp"),
+                        rs.getString("id_Pengguna"),
+                        rs.getString("no_hp"),
                         rs.getString("email"),
                         rs.getString("Username"),
-                        rs.getString("Jenis_lapangan"),
+                        rs.getString("Jenis_langganan"),
                         rs.getString("password")
                     );
                 }
             }
         } catch(Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(e);
+        }
+        return null;
+    }
+    
+    public ArrayList<Pengguna> LoadAll() {
+        ArrayList<Pengguna> penggunas = new ArrayList<Pengguna>();
+        String sql = "SELECT * FROM Pengguna";
+        try (Connection conn = dc.getConnection();PreparedStatement stmt = conn.prepareStatement(sql)) {
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    penggunas.add( new Pengguna(
+                        rs.getString("id_Pengguna"),
+                        rs.getString("no_hp"),
+                        rs.getString("email"),
+                        rs.getString("Username"),
+                        rs.getString("Jenis_langganan"),
+                        rs.getString("password")
+                    ));
+                }
+            }
+        } catch(Exception e) {
+            System.out.println(e);
         }
         return null;
     }
