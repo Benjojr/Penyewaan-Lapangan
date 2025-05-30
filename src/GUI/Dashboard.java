fd2ecf525500200cc096e4db3ded74a2c721587d
Benjojr <ben.jason523@gmail.com>
2025-05-30 21:39:13 +0700

    mengedit tampilan pilihLapangan'

--- src/GUI/Dashboard.java
+++ src/GUI/Dashboard.java
@@ -142,7 +142,8 @@ public class Dashboard extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        new ContainerPilihOlahraga().setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
